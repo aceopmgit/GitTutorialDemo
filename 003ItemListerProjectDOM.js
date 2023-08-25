1fc6778af574854f707f26b076f12e2cec332cf3
aceopmgit <ace.opm.sales@gmail.com>
2023-08-25 12:01:03 +0530

    Asignment Filter

--- 003ItemListerProjectDOM.js
+++ 003ItemListerProjectDOM.js
@@ -1,6 +1,7 @@
 const form=document.getElementById('addForm');
 const itemList=document.getElementById('items');
-//console.log(itemList)
+console.log(itemList)
+const filter=document.getElementById('filter');
 
 //Form Submit event
 form.addEventListener('submit',addItem) // addItem function will run when button is submittted
@@ -8,11 +9,15 @@ form.addEventListener('submit',addItem) // addItem function will run when button
 //Delete Event
 itemList.addEventListener('click',removeItem) //removeItem Function will be called on delete button click
 
+//Filter Event
+filter.addEventListener('keyup',filterItems)//filterItem function will be called from here
+
 //Add Item
 function addItem(e){
     e.preventDefault();
 
     const newItem=document.getElementById('item').value
+    const newItem1=document.getElementById('item1').value;
     
     //Create Element Li on click
     const li=document.createElement('li');
@@ -20,8 +25,9 @@ function addItem(e){
     //Add ClassName
     li.className='list-group-item'
 
-    //Add Text Node with Input value
-    li.appendChild(document.createTextNode(newItem));
+    //Add Text Node with Input values
+    const res1=newItem+' '+newItem1
+    li.appendChild(document.createTextNode(res1));
 
     //create del button element
     const deletebtn=document.createElement('button');
@@ -63,6 +69,38 @@ function removeItem(e){
             itemList.removeChild(li)
         }
     }
+
+}
+
+//Filter Items
+function filterItems(e){
+    //Converts Item to lowerCase so that we do not match lowercase to upperCase
+    const text=e.target.value.toLowerCase();
+    //get li elements from ItemList
+    const items=itemList.getElementsByTagName('li')
+    const desc=
+    //convert to array
+    
+    Array.from(items).forEach(function(item){
+        const itemName=item.firstChild.textContent;
+        //console.log(itemName)
+        if(itemName.toLowerCase().indexOf(text) !=-1){
+            item.style.display='block'
+        }
+        else{
+            item.style.display='none'
+        }
+    })
+    
+   /*
+    for of method
+    let a=Array.from(items);
+   for(let i of a){
+    const ItemName=i.firstChild.textContent
+    console.log(ItemName)
+   }
+   */
+
 
 }
 
