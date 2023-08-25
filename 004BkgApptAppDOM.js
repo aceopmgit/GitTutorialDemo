c7f4589a5da694faf66349bf2f87481dba9c12f4
aceopmgit <ace.opm.sales@gmail.com>
2023-08-25 16:29:27 +0530

    Assignment Lets store the users in the right format

--- 004BkgApptAppDOM.js
+++ 004BkgApptAppDOM.js
@@ -3,9 +3,16 @@ button.addEventListener('click',submitUser)
 
 function submitUser(e){
     e.preventDefault()
-    localStorage.setItem('userName',document.getElementById('name').value);
-    localStorage.setItem('userEmail',document.getElementById('email').value)
+    const details={
+        name:document.getElementById('name').value,
+        email:document.getElementById('email').value
+    }
+    const details_serialised=JSON.stringify(details);
+    localStorage.setItem('details',details_serialised);
+    
+    //console.log(localStorage.getItem('userName'))
 }
+
 
 //this form some functionalities not added  watch the video from 1:30:00 --> https://www.youtube.com/watch?v=hdI2bqOjy3c
 
