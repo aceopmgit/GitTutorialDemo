b061076a5afabddeb13d49fb301df59e43d8a9fc
aceopmgit <ace.opm.sales@gmail.com>
2023-08-26 08:03:30 +0530

    Assignmet Edit Button

--- 004BkgApptAppDOM.js
+++ 004BkgApptAppDOM.js
@@ -17,24 +17,39 @@ function submitUser(e){
     const tEmail=document.getElementById('email').value
     const tPhone=document.getElementById('phone').value
      
-    //combinning the input values
-    const combineT=tName+" "+tEmail+" "+tPhone
+    //Second Method of combining input values
+    //First method is in Item lister Dom    
     
     //Adding TextNode to li
-    li.appendChild(document.createTextNode(combineT));
+    li.appendChild(document.createTextNode(tName));
+    li.appendChild(document.createTextNode(" "+tEmail));
+    li.appendChild(document.createTextNode(" "+tPhone+" "));
 
-    //creating button element
+    //creating delete button element
     const delbtn=document.createElement('button');
+    //creating edit button element
+    const editbtn=document.createElement('button');
     
     //adding class to button
     delbtn.className='btn btn-sm float-right delete'
     delbtn.style.backgroundColor='Tomato'
 
-    //Adding textNode
+    //Adding class and style to Edit Button
+    editbtn.className='btn btn-sm float-right edit'
+    editbtn.style.backgroundColor='#ADD8E6'
+    
+
+    //Adding textNode to delete button
     delbtn.appendChild(document.createTextNode('Delete'));
+
+    //adding text node to Edit button
+    editbtn.appendChild(document.createTextNode('Edit'));
 
     //Adding delete button to li
     li.appendChild(delbtn);
+
+    //Adding edit button to li
+    li.appendChild(editbtn)
 
     //Adding li to itemList
     itemList.appendChild(li);
@@ -54,18 +69,36 @@ function submitUser(e){
 }
 
 function removeInfo(e){
-    //converting text content of item on which delete button is clicked into an anrray to get key i.e.,email
-    let li=e.target.parentElement.textContent.split(' ')[1];
-    //removing item from local storage using key
-    localStorage.removeItem(li);
-    
-    
+        
     if(e.target.classList.contains('delete')){
         if(confirm('Are you Sure ?')){
             let li=e.target.parentElement;
             itemList.removeChild(li);
         }
+        const li=e.target.parentElement.textContent.split(' ')[1];
+        //removing item from local storage using key
+        console.log(li)
+        localStorage.removeItem(li);
     }
+    // Code For Edit Button
+    if(e.target.classList.contains('edit')){
+        let li=e.target.parentElement.textContent.split(' ')[1];
+        let editinfo=JSON.parse(localStorage.getItem(li));
+        //console.log(editinfo.Name)
+
+        localStorage.removeItem(li);
+        let li1=e.target.parentElement;
+        itemList.removeChild(li1);
+        
+        document.getElementById('name').value=editinfo.Name
+        document.getElementById('email').value=editinfo.Email
+        document.getElementById('phone').value=editinfo.Phone
+        
+
+
+    }
+
+
     
     //let delemail=document.getElementById('email').value
 }
