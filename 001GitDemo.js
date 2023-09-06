ea43ebeefadcf9f89759b52f077ddf38c8d9c210
aceopmgit <ace.opm.sales@gmail.com>
2023-09-06 20:39:14 +0530

    new File Added

--- 001GitDemo.js
+++ 001GitDemo.js
@@ -1,1 +1,17 @@
-console.log("hi !!")
+function y(){
+
+
+
+    for(let i=1;i<6;i++){
+    
+    setTimeout(() => console.log(i ), i * 1000)
+    
+    }
+    
+    console.log('Done Coding')
+    
+    
+    
+    }
+    
+    y();
